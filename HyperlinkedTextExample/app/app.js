32296a40b181b5cb86517c9dfcdfdf209a29bd28
Doron Tohar <doron@kimaia.com>
2017-08-13 17:53:14 +0300

    Update example project

--- HyperlinkedTextExample/app/app.js
+++ HyperlinkedTextExample/app/app.js
@@ -17,7 +17,41 @@ export default class HyperlinkedTextExample extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <HyperlinkedText>Browse to https://www.kimaia.com</HyperlinkedText>
+        <HyperlinkedText
+          style={styles.entry}
+        >You get regular URLs handling by default - https://www.kimaia.com</HyperlinkedText>
+        <HyperlinkedText
+          style={styles.entry}
+          linkDefs={[
+            {
+              regex: /\[(.*?)\]\((.*?)\)/mgi,
+              style: {color: 'red'},
+              replaceText: (orig, text, url) => text,
+              onPress: (orig, text, url) => HyperlinkedText._openWebUrl(url)
+            }
+          ]}
+        >Use markdown style links [Kimaia](https://www.kimaia.com)</HyperlinkedText>
+        <HyperlinkedText
+          style={styles.entry}
+                    linkDefs={[
+            {
+              regex: /([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)/mgi,
+              style: {color: 'green'},
+              replaceText: email => `[email to: ${email}]`
+            },
+            {
+              regex: /@\[(.*?)\]\((.*?)\)/mgi,
+              style: {color: 'purple'},
+              replaceText: (orig, name, id) => name,
+              onPress: (orig, name, id) => window.alert(`name: ${name}, id: ${id}`)
+            },
+            {
+              regex: /\d+:\d+(?:\d+)?/mgi,
+              style: {color: '#0999aa', borderWidth: 1, borderColor: 'red'},
+              noPress: true
+            }
+          ]}
+        >Check for email someone@kimaia.com, or app specific stuff like mentions - @[Doron Tohar](1234) or any regex, e.g. 10:00.</HyperlinkedText>
       </View>
     );
   }
@@ -25,20 +59,15 @@ export default class HyperlinkedTextExample extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1,    
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
+  entry: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 15,
   },
 });
 
