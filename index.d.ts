142189b727ca5ecb5980da66e8f07b64b38f136e
Sergei Butko <svbutko@hotmail.com>
2018-10-10 12:53:20 +0300

    Add additional type definitions

--- index.d.ts
+++ index.d.ts
@@ -7,12 +7,14 @@ declare module "react-native-hyperlinked-text" {
         style?: TextStyle;
         replaceText?: (orig: string, text: string, url: string) => string;
         onPress?: (orig: string, text: string, url: string) => void;
+        noPress?: boolean;
     }
 
     interface IProps {
         style?: TextStyle;
+        linkStyle?: TextStyle;
         linkDefs?: ILinkDef[];
-
+        onLinkPress?: (text: string) => void;
     }
 
     export default class HyperlinkedText extends React.Component<IProps, any> {
